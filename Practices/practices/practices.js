58d36bcbf9b933ff2b324a3bbd482eeff1f45780
ProDeveloper-1 <prospermbuma@gmail.com>
2021-03-25 16:20:09 +0300

    Added some webs

---
+++ Practices/practices/practices.js
@@ -0,0 +1,21 @@
+function firstName() {
+    console.log('Prosper');
+}
+
+function secondName() {
+    console.log('Nicholas');
+}
+
+function lastName() {
+    console.log('Mbuma');
+}
+
+function myNameis() {
+    firstName();
+    secondName();
+    lastName();
+}
+
+myNameis();
+
+console.log('That\'s me, thanks!');
